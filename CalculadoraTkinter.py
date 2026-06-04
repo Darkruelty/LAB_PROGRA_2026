1f4cfa0b2540717d4f2517bce1853c3939a7a114
Darkruelty <darkruelty@gmail.com>
2026-06-03 22:57:43 -0600

    AGREGO LOS BONTES ( )

--- CalculadoraTkinter.py
+++ CalculadoraTkinter.py
@@ -30,7 +30,7 @@ def realizar_operacion():
         entrada.delete(0, tk.END)
         entrada.insert(tk.END, "Error")
 
-# Función para limpiar la entrada
+   
 
 def limpiar():
     entrada.delete(0, tk.END)  # Borra todo el contenido del campo de entrada
@@ -49,8 +49,10 @@ botones = [
     ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("/", 1, 3),
     ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("*", 2, 3),
     ("1", 3, 0), ("2", 3, 1), ("3", 3, 2), ("-", 3, 3),
-    ("0", 4, 0), (".", 4, 1), ("**", 4, 2), ("+", 4, 3)
-]
+    ("0", 4, 0), (".", 4, 1), ("**", 4, 2), ("+", 4, 3),
+    ("(", 5, 0), (")", 5, 1)
+
+    ]
 
 
 # Creamos los botones y ubicamos en la cuadrícula
@@ -58,13 +60,14 @@ for valor, fila, columna in botones:
     boton = tk.Button(ventana, text=valor, font=("Arial", 20), command=lambda v=valor: agregar_numero(v))
     boton.grid(row=fila, column=columna, padx=5, pady=6, sticky="nsew")
 
+
 # Creamos el botón para limpiar el campo de entrada
 limpiar_boton = tk.Button(ventana, text="C", font=("Arial", 20), command=limpiar)
-limpiar_boton.grid(row=5, column=0, padx=5, pady=5, columnspan=2, sticky="nsew")
+limpiar_boton.grid(row=5, column=2, padx=5, pady=5, columnspan=1, sticky="nsew")
 
 # Creamos el botón de igual para obtener el resultado de la operación
 calcular_boton = tk.Button(ventana, text="=", font=("Arial", 20), command=realizar_operacion)
-calcular_boton.grid(row=5, column=2, padx=5, pady=5, columnspan=2, sticky="nsew")
+calcular_boton.grid(row=5, column=3, padx=5, pady=5, columnspan=2, sticky="nsew")
 
 #iniciamos el bucle principal de la aplicación
 ventana.mainloop()
